25f7badd174028bd66d6c647fb2e86033ba9d839
lschimm <lschimmelfennig@gmail.com>
2019-05-08 16:05:29 -0500

    finish explaing 'this'

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  (Window/Global Object Binding) In the global scope, "this" will have a larger scop as well (window/console Object;)
+* 2. (Implicit Binding) "this" will be the object before the dot when in a function
+* 3. (New Binding) ""
+* 4. (Explicit binding)""
 *
 * write out a code example of each explanation above
 */
